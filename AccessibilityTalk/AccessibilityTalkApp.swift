3d36b78453f911f8e5bde555bb55ce52f8b91174
Sami Samhuri <sami@samhuri.net>
2024-02-11 14:02:34 -0800

    Initial Commit

---
+++ AccessibilityTalk/AccessibilityTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityTalkApp.swift
+//  AccessibilityTalk
+//
+//  Created by Work on 2024-02-11.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
